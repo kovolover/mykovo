894acf0d1a952d826c25e688f27593b00c75d363
kovolover <163205976+kovolover@users.noreply.github.com>
2024-03-24 16:37:30 +0700

    Update web.php

--- api/web.php
+++ api/web.php
@@ -66,7 +66,15 @@ table, th, td {
   font-size: 12px;
 }
 </style>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3HQE1XC5NZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-3HQE1XC5NZ');
+</script>
 </head>
 <body style="font-family:Verdana;">
 
